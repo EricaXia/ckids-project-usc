fcfce60b38b6cf0f94ad038496cea4dce5e12c7c
Erica Xia <29347801+EricaXia@users.noreply.github.com>
2020-02-18 17:52:02 -0800

    update loc scrape

--- ig_location_1.py
+++ ig_location_1.py
@@ -1,5 +1,6 @@
 """ 
-(Can find Loc ID in web URL on Instagram location page)
+(Can find Loc ID in web URL on Instagram location page,
+or check the database file link in README)
 
 Popular Location IDS in LA:
 212999109 - Los Angeles
@@ -14,8 +15,8 @@ L = instaloader.Instaloader()
 
 L.login('musketeers1128@gmail.com', 'EvOu7CR7Iq2V')
 
-L.download_location(212999109,
-                    max_count=30,
+L.download_location("1865514327051885",
+                    max_count=15,
                     # filter by specific hashtag in post's caption
                     post_filter=lambda post: "food" in post.caption_hashtags,
                     fast_update=True)
